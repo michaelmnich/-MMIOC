df4f57947da256908d9358ba6d18961f883fe3bf
michaelmnich <michael.mnich@gmail.com>
2019-07-10 00:55:06 +0200

    Add project files.

---
+++ MMIOC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+///  MMIOC - Is prototype of platform that, implements manny mutants in one compilation 
+/// </summary>
+namespace MMIOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
