8b3ae9f9bb9b53d793a5292839689a225bc45b80
michaelmnich <michael.mnich@gmail.com>
2019-07-15 00:18:19 +0200

    sample 02 code

--- MMIOC/CodeSamples/sample02.cpp
+++ MMIOC/CodeSamples/sample02.cpp
@@ -8,6 +8,7 @@ void bubbleSort(int arr[], int n);
 void swap(int* xp, int* yp);
 void printArray(int arr[], int size);
 void selectionSort(int arr[], int n);
+void quicksort(int tablica[], int p, int r);
 
 // Driver program to test above functions 
 int main()
@@ -17,12 +18,24 @@ int main()
 	bubbleSort(arr, n);
 	cout << "BUBLE Sorted array: ";
 	printArray(arr, n);
-	cout << endl << "------------------------------------------------\n" << endl;
+	cout << endl << "------------------------------------------------" << endl;
 	int arr2[] = { 64, 25, 12, 22, 11 };
 	int n2 = sizeof(arr2) / sizeof(arr2[0]);
 	selectionSort(arr2, n2);
-	cout << "SELECTION Sorted array: \n";
+	cout << "SELECTION Sorted array:";
 	printArray(arr2, n2);
+
+
+
+	int arr3[] = { 62, 21, 25, 4, 22, 11, 8 };
+	quicksort(arr3, 0, 6); // wywolanie funkcji sortujacej
+	cout << endl << "------------------------------------------------" << endl;
+	cout << "QUICK Sorted array:";
+	printArray(arr3, 6);
+	//for (int i = 0; i < 6; i++) // wypisanie posortowanej tablicy
+	//	cout << "tablica[" << i << "] = " << arr3[i] << endl;
+
+
 	return 0;
 }
 
@@ -74,7 +87,7 @@ void printArray(int arr[], int size)
 
 }
 
-
+//quicksort----------------------------------------------------------------------------------------------------------
 void selectionSort(int arr[], int n)
 {
 	int i;
@@ -101,7 +114,39 @@ void selectionSort(int arr[], int n)
 	}
 }
 
+int partition(int tablica[], int p, int r) // dzielimy tablice na dwie czesci, w pierwszej wszystkie liczby sa mniejsze badz rowne x, w drugiej wieksze lub rowne od x
+{
+	int x = tablica[p]; // obieramy x
+	int i = p, j = r, w; // i, j - indeksy w tabeli
+	while (true) // petla nieskonczona - wychodzimy z niej tylko przez return j
+	{
+		while (tablica[j] > x) // dopoki elementy sa wieksze od x
+			j--;
+		while (tablica[i] < x) // dopoki elementy sa mniejsze od x
+			i++;
+		if (i < j) // zamieniamy miejscami gdy i < j
+		{
+			w = tablica[i];
+			tablica[i] = tablica[j];
+			tablica[j] = w;
+			i++;
+			j--;
+		}
+		else // gdy i >= j zwracamy j jako punkt podzialu tablicy
+			return j;
+	}
+}
 
+void quicksort(int tablica[], int p, int r) // sortowanie szybkie
+{
+	int q;
+	if (p < r)
+	{
+		q = partition(tablica, p, r); // dzielimy tablice na dwie czesci; q oznacza punkt podzialu
+		quicksort(tablica, p, q); // wywolujemy rekurencyjnie quicksort dla pierwszej czesci tablicy
+		quicksort(tablica, q + 1, r); // wywolujemy rekurencyjnie quicksort dla drugiej czesci tablicy
+	}
+}
 
 
 // This is code is contributed by rathbhupendra 
